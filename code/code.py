cc7222179b4070cfc684bd3ad858d83918cd0e63
Dinesh12 <55702844+dineshkannan010@users.noreply.github.com>
2024-08-25 21:30:57 +0000

    Update code.py

--- code/code.py
+++ code/code.py
@@ -3,7 +3,7 @@ import random
 # Generate a list of 10 random numbers between 1 and 100
 random_numbers = [random.randint(1, 100) for _ in range(10)]
 
-print("Original list:" random_numbers)
+print("Original list:", random_numbers)
 
 # Sort the list in ascending order
 sorted_numbers = sorted(random_numbers)
