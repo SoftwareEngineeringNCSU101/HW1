788b27cd3db7527612e8e75d607e496c4bbadc6b
Dinesh12 <dineshkannan010@gmail.com>
2024-08-25 18:41:16 -0400

    Added testcases for main file

--- test/test_code.py
+++ test/test_code.py
@@ -1,5 +1,13 @@
-from code.code import code
+import sys
+import os
+
+# Add the root directory of the project to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.main import average
 
 def test_average():
-    assert code.average([2,4])== 3
-    assert code.average([4,6])== 4
+    assert average([2,4]) == 3
+
+def test_average2():
+    assert average([4,6]) == 4
